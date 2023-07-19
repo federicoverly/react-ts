d9de33ae4694297cf45f776016e1ee4d3925dac2
Federico Verly <federicoverly@gmail.com>
2023-07-19 15:29:06 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ReactNode, useCallback } from "react";
+import "./App.css";
+
+const Heading = ({ title }: { title: string }) => {
+  return <h2>{title}</h2>;
+};
+
+const Box = ({ children }: { children: ReactNode }) => {
+  return <div style={{ padding: "1rem", fontWeight: "bold" }}>{children}</div>;
+};
+
+const List = ({
+  items,
+  onClick,
+}: {
+  items: string[];
+  onClick?: (item: string) => void;
+}) => {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index} onClick={() => onClick?.(item)}>
+          {item}
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 function App() {
+  const onListClick = useCallback((item: string) => {
+    alert(item);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading title="Introduction" />
+      <Box>Hello there</Box>
+      <List items={["one", "two", "three"]} onClick={onListClick} />
     </div>
   );
 }
